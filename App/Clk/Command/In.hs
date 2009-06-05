f39559508a3575798c45793f8569a13500d3315b
Michael Hendricks <michael@ndrix.org>
2009-06-05 14:56:49 -0600

    in: determine the tags from command line flags

--- App/Clk/Command/In.hs
+++ App/Clk/Command/In.hs
@@ -4,13 +4,18 @@ import App.Clk.Storage (Storage, insert)
 import App.Clk.Config (get_user_entity, open_default_storage)
 import Data.Time (getCurrentTime)
 import System.Console.GetOpt
+import Data.Maybe (mapMaybe)
 
-data Flag = Subject String | SecondFlagThatsNeverUsed
+data Flag = Subject String | Tag String
 options :: [ OptDescr Flag ]
 options =
     [
         Option ['s'] ["subject"] (ReqArg Subject "SUBJECT") "event subject"
+    ,   Option ['t'] ["tag"]     (ReqArg Tag     "TAG"    ) "event tags"
     ]
+
+isTag ( Tag _ ) = True
+isTag ( _     ) = False
 
 -- This command creates a new event on a timeline.  It's the main
 -- technique for tracking one's time.
@@ -19,13 +24,19 @@ run argv = do
     putStrLn "running 'in'"
     store <- open_default_storage
     let (args, extra, error) = getOpt Permute options argv
-    command_in store (subject args) ["first", "second"]
+    command_in store (subject args) (tags args)
 
 -- determine the subject from the command line flags
-subject :: [Flag] -> String
+subject :: [Flag] -> Subject
 subject [] = ""
 subject (  Subject s : _  ) = s
 subject (  _         : xs ) = subject xs
+
+-- determine the tags from the command line flags
+tags :: [Flag] -> Tags
+tags = mapMaybe p
+    where p ( Tag t ) = Just t
+          p _         = Nothing
 
 -- This is a rough outline for how the 'clk in' command might be implemented.
 -- Presumably there's something at a higher level that parses the command line
