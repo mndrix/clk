4c0082508d4885a12109d624c86188b7bf82938d
Michael Hendricks <michael@ndrix.org>
2009-06-05 13:12:12 -0600

    Simplify the source tree
    Standardize on .hs and remove the Null storage implementation

--- App/Clk/Config.hs
+++ App/Clk/Config.hs
@@ -1,7 +1,7 @@
 module App.Clk.Config where
 import App.Clk (Entity)
 import App.Clk.Storage (Storage)
-import App.Clk.Storage.Null (StorageNull, empty)
+import App.Clk.Storage.Memory (StorageMemory, empty)
 
 -- This user's entity is most likely stored in a configuration file or an
 -- environment variable, so we'll need IO to retrieve it.
@@ -11,5 +11,5 @@ get_user_entity = do
   putStrLn "getting user entity"
   return "michael@ndrix.org"
 
-open_default_storage :: IO StorageNull
-open_default_storage = return App.Clk.Storage.Null.empty
+open_default_storage :: IO StorageMemory
+open_default_storage = return App.Clk.Storage.Memory.empty
