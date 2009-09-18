a82c3575e08ffb64ce0e8079277096fac3fe4607
Michael Hendricks <michael@ndrix.org>
2009-09-18 14:44:59 -0600

    Better error message for non-executable hooks
    If a hook script cannot be executed, produce a more useful error message
    than the one Haskell provides itself (something about hFlush).

--- App/Clk/Util.hs
+++ App/Clk/Util.hs
@@ -1,9 +1,16 @@
 module App.Clk.Util where
 import App.Clk.Config
 import System.Process
+import System.Posix.Files
 
 -- Runs a hook script
 run_hook :: String -> [String] -> String -> IO String
 run_hook hook args input = do
     hookPath <- clkPath $ "hooks/" ++ hook
-    readProcess hookPath args input
+    canExec <- isExecutable hookPath
+    if not canExec
+        then error $ "Hook '" ++ hookPath ++ "' is not executable"
+        else readProcess hookPath args input
+
+isExecutable :: FilePath -> IO Bool
+isExecutable p = fileAccess p False False True
