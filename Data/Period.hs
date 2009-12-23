d7401ac1a2c054af733c0db19459a456cf4dd059
Michael Hendricks <michael@ndrix.org>
2009-12-22 20:18:05 -0700

    Implement period phrase 'ever'

--- Data/Period.hs
+++ Data/Period.hs
@@ -1,6 +1,7 @@
 module Data.Period where
 import Data.Time.Clock
 import Data.Time.LocalTime
+import Data.Time.Calendar
 
 data Period = Period UTCTime UTCTime
 
@@ -9,6 +10,8 @@ parsePeriod "today" = do
     utc   <- getCurrentTime
     tz    <- getTimeZone utc
     return $ enclosingDay tz $ utcToLocalTime tz utc
+parsePeriod "ever" = return $ Period (day 0) (day (2^16))
+        where day = flip UTCTime (secondsToDiffTime 0) . ModifiedJulianDay
 parsePeriod p = error $ "Cannot parse period string '" ++ p ++ "'"
 
 enclosingDay :: TimeZone -> LocalTime -> Period
