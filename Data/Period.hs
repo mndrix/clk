4495c00873e3f2f96b8fa171bae0f25ed06788f0
Michael Hendricks <michael@ndrix.org>
2009-12-23 22:17:54 -0700

    Support "this week" period description
    This code has quite a bit of duplication, but it does correctly handle
    the period 'this week'.
    
    Closes gh-3

--- Data/Period.hs
+++ Data/Period.hs
@@ -3,6 +3,7 @@ import Data.Time.Clock
 import Data.Time.Format (formatTime)
 import Data.Time.LocalTime
 import Data.Time.Calendar
+import Data.Time.Calendar.OrdinalDate (mondayStartWeek)
 import System.Locale (defaultTimeLocale)
 
 data Period = Period UTCTime UTCTime
@@ -18,6 +19,10 @@ parsePeriod "today" = do
     utc   <- getCurrentTime
     tz    <- getTimeZone utc
     return $ enclosingDay tz $ utcToLocalTime tz utc
+parsePeriod "this week" = do
+    utc   <- getCurrentTime
+    tz    <- getTimeZone utc
+    return $ enclosingWeek tz $ utcToLocalTime tz utc
 parsePeriod "ever" = return $ Period (day 0) (day (2^16))
         where day = flip UTCTime (secondsToDiffTime 0) . ModifiedJulianDay
 parsePeriod p = error $ "Cannot parse period string '" ++ p ++ "'"
@@ -27,3 +32,12 @@ enclosingDay tz ( LocalTime day time ) = Period utcBegin utcEnd
     where
         utcBegin = localTimeToUTC tz $ LocalTime day $ TimeOfDay 0 0 0
         utcEnd   = localTimeToUTC tz $ LocalTime day $ TimeOfDay 23 59 59
+
+enclosingWeek :: TimeZone -> LocalTime -> Period
+enclosingWeek tz ( LocalTime day _ ) = Period utcBegin utcEnd
+    where
+        dayOfWeek = fromIntegral $ snd $ mondayStartWeek day
+        monday   = addDays (1-dayOfWeek) day
+        sunday   = addDays (7-dayOfWeek) day
+        utcBegin = localTimeToUTC tz $ LocalTime monday $ TimeOfDay 0 0 0
+        utcEnd   = localTimeToUTC tz $ LocalTime sunday $ TimeOfDay 23 59 59
