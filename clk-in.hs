cc7b09934475bf363b87130383d064b579c5829f
Michael Hendricks <michael@ndrix.org>
2010-05-14 15:15:43 -0600

    Save entries to a file

--- clk-in.hs
+++ clk-in.hs
@@ -9,15 +9,16 @@ import System.Environment
 import System.Locale
 
 main = do
-    -- TODO find the current time
+    -- display an entry line
     now <- getCurrentTime
     msg <- fmap (intercalate " ") getArgs
-    putStrLn $ entryLine "michael@ndrix.org" now [] msg
+    let line = entryLine "michael@ndrix.org" now [] msg
+    putStrLn line
 
+    -- save the entry to disk
     clkDir <- getClkDir
-    putStrLn $ folder clkDir now
-    -- TODO which month folder to use
-    -- TODO save the entry line to the file
+    let file = folder clkDir now
+    appendFile file (line++"\n")
 
 entryLine :: String -> UTCTime -> [String] -> String -> String
 entryLine e t ts m = intercalate "\t" [e,ymd,tags,m]
@@ -29,8 +30,11 @@ strftime = formatTime defaultTimeLocale
 getClkDir :: IO String
 getClkDir = do
     home <- getEnv "HOME"
-    return $ home ++ "/.clk/"
+    return $ home ++ "/.clkq/"  -- q avoids collision with older data
 
 folder :: String -> UTCTime -> String
 folder clkDir t = intercalate "" [clkDir,"timeline/",file]
     where file = strftime "%Y-%m.txt" t
+
+-- init :: String -> IO ()
+-- rough equivalent of `mkdir -p ~/.clkq/timeline`
