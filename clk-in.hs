0215eddb53ece5db00b11c21c973b2c6c807fa4a
Michael Hendricks <michael@ndrix.org>
2010-05-14 14:50:31 -0600

    Factor out entry line creation

--- clk-in.hs
+++ clk-in.hs
@@ -1,5 +1,19 @@
 -- This is intended to a very quick script being the simplest thing
 -- that could possibly work
 
+import Data.Time
+import System.Locale
+import Data.List
+
 main = do
-    putStrLn "Just starting"
+    -- TODO find the current time
+    now <- getCurrentTime
+    putStrLn $ entryLine "michael@ndrix.org" now [] ""
+    -- TODO find the user's home directory
+    -- TODO which month folder to use
+    -- TODO save the entry line to the file
+
+entryLine :: String -> UTCTime -> [String] -> String -> String
+entryLine e t ts m = intercalate "\t" [e,ymd,tags,m]
+    where tags = intercalate "," ts
+          ymd  = formatTime defaultTimeLocale "%FT%TZ" t
