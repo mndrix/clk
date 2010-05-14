8b2f09d01c3c354c908406de6c26d7ec1ecaab03
Michael Hendricks <michael@ndrix.org>
2010-05-14 15:09:43 -0600

    Calculate where entry lines should go

--- clk-in.hs
+++ clk-in.hs
@@ -1,19 +1,36 @@
 -- This is intended to a very quick script being the simplest thing
 -- that could possibly work
 
-import Data.Time
-import System.Locale
 import Data.List
+import Data.Time
+import Data.Time.Clock
+import Data.Time.Calendar
+import System.Environment
+import System.Locale
 
 main = do
     -- TODO find the current time
     now <- getCurrentTime
-    putStrLn $ entryLine "michael@ndrix.org" now [] ""
-    -- TODO find the user's home directory
+    msg <- fmap (intercalate " ") getArgs
+    putStrLn $ entryLine "michael@ndrix.org" now [] msg
+
+    clkDir <- getClkDir
+    putStrLn $ folder clkDir now
     -- TODO which month folder to use
     -- TODO save the entry line to the file
 
 entryLine :: String -> UTCTime -> [String] -> String -> String
 entryLine e t ts m = intercalate "\t" [e,ymd,tags,m]
     where tags = intercalate "," ts
-          ymd  = formatTime defaultTimeLocale "%FT%TZ" t
+          ymd  = strftime "%FT%TZ" t
+
+strftime = formatTime defaultTimeLocale
+
+getClkDir :: IO String
+getClkDir = do
+    home <- getEnv "HOME"
+    return $ home ++ "/.clk/"
+
+folder :: String -> UTCTime -> String
+folder clkDir t = intercalate "" [clkDir,"timeline/",file]
+    where file = strftime "%Y-%m.txt" t
