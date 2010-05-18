ead6d71224db990b137823a9b497513bb3818862
Michael Hendricks <michael@ndrix.org>
2010-05-18 15:14:24 -0700

    Implement a split function
    This function splits a string into multiple smaller strings based on a
    delimiter character.

--- App/Clk/Util.hs
+++ App/Clk/Util.hs
@@ -7,3 +7,8 @@ getClkDir = do
     home <- getEnv "HOME"
     return $ home ++ "/.clkq/"  -- q avoids collision with older data
 
+split :: Char -> String -> [String]
+split delim s
+    | [] == rest = [token]
+    | otherwise = token : split delim (tail rest)
+    where (token,rest) = span (/=delim) s
