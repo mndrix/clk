dde163b8d2f3a9ed2e5542a5329d350c7dbfc5a9
Michael Hendricks <michael@ndrix.org>
2010-05-19 16:10:39 -0700

    Make Entry a custom Show instance
    The instance from "deriving" was there only for debugging.  This
    implementation does the same thing as clk-in.  The duplication between
    those two places should be factored out soon.
    
    At this point, I've made the decision that show and read deal with the
    filesystem representation of an Entry.  That seems reasonable since the
    two are inverses of each other.  Displaying an Entry for user
    consumption only needs to go one way.

--- clk-ls.hs
+++ clk-ls.hs
@@ -1,4 +1,5 @@
 import App.Clk.Util
+import Data.List
 import Data.Time.Clock
 import System.Directory
 import Text.Regex.Posix
@@ -7,13 +8,18 @@ type Name    = String
 type Tags    = [String]
 type Message = String
 data Entry   = Entry Name UTCTime Tags Message
-    deriving Show
 
 instance Read Entry where
     readsPrec _ line = [( Entry name time tags msg, "" )]
         where [name,timeS,tagsS,msg] = split '\t' line
               tags = split ',' tagsS
               time = strptime iso8601 timeS
+
+instance Show Entry where
+    show (Entry name time tags msg) = intercalate "\t" parts
+        where parts = [ name, timeS, tagsS, msg ]
+              timeS = strftime iso8601 time
+              tagsS = intercalate "," tags
 
 main = do
     now <- getCurrentTime
