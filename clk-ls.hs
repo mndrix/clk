4f380ee8660401caa56cb689249d2f5cf6211564
Michael Hendricks <michael@ndrix.org>
2010-05-19 21:15:52 -0700

    Display `ls` output in the local time zone
    The user rarely wants to see his entries in UTC time.  Instead, he wants
    to know what time he started working on something in his own local time.
    He's much more likely to be able to correlate that to something in his
    personal experience.  That suggests that local time carries more
    information somehow (maybe just more accessible information).

--- clk-ls.hs
+++ clk-ls.hs
@@ -2,14 +2,16 @@ import App.Clk.Entry
 import App.Clk.Util
 import Data.List
 import Data.Time.Clock
+import Data.Time.LocalTime
 import System.Directory
 import Text.Regex.Posix
 
 main = do
     now <- getCurrentTime
+    tz     <- getCurrentTimeZone
     clkDir <- getClkDir
     entries <- mostRecentMonthEntries clkDir
-    putStrLn $ intercalate "\n" $ map showUser entries
+    putStrLn $ intercalate "\n" $ map (showUser tz) entries
 
 isMonthFile :: FilePath -> Bool
 isMonthFile p = p =~ "^[0-9]{4}-[0-9]{2}.txt$"
@@ -23,8 +25,8 @@ mostRecentMonthEntries clkDir = do
                 content <- readFile $ clkDir ++ "timeline/" ++ p
                 return $ map read $ lines content
 
-showUser :: Entry -> String
-showUser (Entry name time tags msg dur) = intercalate "\t" parts
+showUser :: TimeZone -> Entry -> String
+showUser tz (Entry name time tags msg dur) = intercalate "\t" parts
     where parts = [ userTime, tagsS, msg ]
-          userTime = strftime "%m/%d %H:%M" time
+          userTime = strftime "%m/%d %H:%M" $ utcToLocalTime tz time
           tagsS    = intercalate "," tags
