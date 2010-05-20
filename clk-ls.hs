c743910e1208dd8cb27474b938ffda4f65cef30b
Michael Hendricks <michael@ndrix.org>
2010-05-20 15:30:11 -0700

    Include duration in `ls` output
    For each entry, calculate how long that entry lasted (based on the
    entry's context) and display that duration in a user-friendly,
    high-level summary sort of way.
    
    This code has one *significant* bug.  It doesn't display a line for the
    final entry.  That needs to be fixed.  It's important to be able to see
    the most recent entry.

--- clk-ls.hs
+++ clk-ls.hs
@@ -11,7 +11,7 @@ main = do
     tz     <- getCurrentTimeZone
     clkDir <- getClkDir
     entries <- mostRecentMonthEntries clkDir
-    putStrLn $ intercalate "\n" $ map (showUser tz) entries
+    putStrLn $ intercalate "\n" $ map (showUser tz) $ tween setDuration entries
 
 isMonthFile :: FilePath -> Bool
 isMonthFile p = p =~ "^[0-9]{4}-[0-9]{2}.txt$"
@@ -27,11 +27,23 @@ mostRecentMonthEntries clkDir = do
 
 showUser :: TimeZone -> Entry -> String
 showUser tz (Entry name time tags msg dur) = intercalate "\t" parts
-    where parts = [ userTime, tagsS, msg ]
+    where parts = [ userTime, durS, tagsS, msg ]
           userTime = strftime "%m/%d %H:%M" $ utcToLocalTime tz time
           tagsS    = intercalate "," tags
+          durS     = maybe "" showDur dur
+
+setDuration :: Entry -> Entry -> Entry
+setDuration e0 e1 = e0{ dur = Just diffSeconds }
+    where diffSeconds = diffUTCTime (time e1) (time e0)
 
 tween :: ( a -> a -> b ) -> [a] -> [b]
 tween _ [ ] = []
 tween _ [x] = []
 tween f (x:y:xs) = (x `f` y):(tween f (y:xs))
+
+showDur :: NominalDiffTime -> String
+showDur dur = case dur of
+            x | x <       60 -> show (round x) ++ "s"
+              | x <    60*60 -> show (round (x/60)) ++ "m"
+              | x < 24*60*60 -> show (round (x/60/60)) ++ "h"
+            x                -> show (round (x/24/60/60)) ++ "d"
