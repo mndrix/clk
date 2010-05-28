0081aa355411ec47375e3f3e5c5961b099822b53
Michael Hendricks <michael@ndrix.org>
2010-05-28 10:58:33 -0600

    Give `ls` a period argument
    The previous behavior was to list all entries within the current month.
    That's rarely the desired behavior.  Listing today's entries is much
    more commonly useful and being able to specify a specific time period is
    really handy.

--- clk-ls.hs
+++ clk-ls.hs
@@ -1,10 +1,23 @@
 import App.Clk.Entry
 import App.Clk.Util
 import Data.List
+import Data.Period
 import Data.Time.Clock
 import Data.Time.LocalTime
+import System.Console.GetOpt
+import System.Environment
+
+data Flag = PeriodArg String -- -p or --period
+options = [ Option ['p'] ["period"] (ReqArg PeriodArg "PERIOD") ""
+          ]
 
 main = do
+    args <- getArgs
+    let periodPhrase = case getOpt Permute options args of
+                            ([PeriodArg p], [], []) -> p
+                            otherwise               -> "today"
+    period <- parsePeriod periodPhrase
+
     tz     <- getCurrentTimeZone
-    entries <- mostRecentMonthEntries
+    entries <- fmap (filter $ isWithin period) mostRecentMonthEntries
     putStrLn $ intercalate "\n" $ map (showUser tz) $ entries
