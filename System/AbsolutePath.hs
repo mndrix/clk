c9329556f712d3116fa89ebe4eb704b5e43e2e7f
Michael Hendricks <michael@ndrix.org>
2010-06-18 17:05:06 -0600

    Start of System.AbsolutePath
    This type is a restriction of FilePath which guarantees that the path is
    absolute.  Perhaps there are better ways of handling this, but I find it
    conceptually valuable to know that a path I have is guaranteed to be
    absolute and I can use it without regard to the current directory or a
    specific base directory.
    
    A couple functions implemented by FilePath are also implemented.  They
    just pass through to the FilePath function.  It'd sure be handy if
    Haskell had subtypes like Moose.  Then I could do
    
        subtype 'AbsolutePath'
            => as 'FilePath'
            => where { isAbsolute $_ }
            => message { "The path $_ is not absolute" }

---
+++ System/AbsolutePath.hs
@@ -0,0 +1,21 @@
+module System.AbsolutePath (
+      AbsolutePath
+    , mkAbsolutePath
+    , takeBaseName
+    , takeExtension
+) where
+import qualified System.FilePath as FP
+
+data AbsolutePath = AbsolutePath { path :: FP.FilePath }
+    deriving (Show, Eq)
+
+mkAbsolutePath :: FP.FilePath -> AbsolutePath
+mkAbsolutePath fp
+    | FP.isAbsolute fp = AbsolutePath fp
+    | otherwise = error "Can't build an AbsolutePath from a relative path"
+
+takeBaseName :: AbsolutePath -> String
+takeBaseName = FP.takeBaseName . path
+
+takeExtension :: AbsolutePath -> String
+takeExtension = FP.takeExtension . path
