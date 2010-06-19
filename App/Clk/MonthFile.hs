50f457e574210945394882d0a8a5cdd1ffac1c8f
Michael Hendricks <michael@ndrix.org>
2010-06-19 11:21:41 -0600

    Start on App.Clk.MonthFile
    This type represents files on disk which contain clk entries for an
    entire month.  It's convenient to bundle together the absolute path to
    the file (so it can be read or written to) and the time period covered
    by the file (so it can be discarded quickly during searches).
    
    The main interface to this type is maybeMonthFile which takes an
    absolute path which might represent a MonthFile and possibly constructs
    a value of that type if it is.

---
+++ App/Clk/MonthFile.hs
@@ -0,0 +1,54 @@
+module App.Clk.MonthFile (
+      MonthFile
+    , maybeMonthFile
+) where
+
+import Data.Period (Period(Period))
+import Data.Time.Calendar
+import Data.Time.Clock
+import System.AbsolutePath
+import Text.Regex.Posix
+
+data MonthFile = MonthFile Period AbsolutePath
+
+maybeMonthFile :: AbsolutePath -> Maybe MonthFile
+maybeMonthFile path
+    | isMonthFile path = Just $ MonthFile period path
+    | otherwise        = Nothing
+        where period = pathPeriod path
+
+pathPeriod :: AbsolutePath -> Period
+pathPeriod path = Period monthStart monthEnd
+    where monthStart = firstSecond month
+          monthEnd   = lastSecond  month
+          month      = mkMonth $ takeBaseName path
+
+isMonthFile :: AbsolutePath -> Bool
+isMonthFile path = name && extension
+    where name      = isYYYYMM   $ takeBaseName  path
+          extension = (==".txt") $ takeExtension path
+
+isYYYYMM :: String -> Bool
+isYYYYMM x = x =~ "^[0-9]{4}-[0-9]{2}$"
+
+
+
+-- a convenient type when working with MonthFile values
+data Month = Month { year :: Integer, month :: Int }
+mkMonth :: String -> Month
+mkMonth yyyymm = Month (read yyyy) (read mm)
+    where yyyy = take 4 yyyymm
+          mm   = drop 5 yyyymm
+
+firstSecond :: Month -> UTCTime
+firstSecond m = UTCTime day time
+    where day  = fromGregorian (year m) (month m) 1
+          time = secondsToDiffTime 0
+
+lastSecond :: Month -> UTCTime
+lastSecond m = UTCTime day time
+    where day  = fromGregorian (year m) (month m) (lastDay m)
+          time = secondsToDiffTime 86400
+
+lastDay :: Month -> Int
+lastDay m = gregorianMonthLength (year m) (month m)
